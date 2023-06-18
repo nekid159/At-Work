59da9e129209eea9fd9d64cb291ebdcb5f73e352
nekid159 <123189562+nekid159@users.noreply.github.com>
2023-06-18 12:57:23 +0300

    раскидал

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -6,51 +6,10 @@ const AuthPage = () => {
         <div className="App">
             <div className={styles.back}>
                 <div className={styles.square}>
-                    <input
-                        type="text"
-                        placeholder="Введите имя пользователя"
-                        style={{
-                            width: "300px",
-                            height: "46px",
-                            borderRadius: "6px",
-                            padding: "12px",
-                            gap: "6px",
-                            boxSizing: "border-box",
-                            border: "1px solid #D8DEEB",
-                            flex: "none",
-                            order: "0",
-                            flexGrow: "0",
-                        }}
+                    <input className={styles.email} type="text" placeholder="Введите имя пользователя"                    
                     />
-
-                    <div
-                        style={{
-                            display: "flex",
-                            flexDirection: "column",
-                            alignItems: "center",
-                            marginTop: "20px",
-                        }}
-                    >
-                        <button
-                            style={{
-                                display: "flex",
-                                flexDirection: "row",
-                                justifyContent: "center",
-                                alignItems: "center",
-                                padding: "12px",
-                                gap: "14px",
-                                width: "300px",
-                                height: "46px",
-                                background: "#107EDC",
-                                borderRadius: "8px",
-                                flex: "none",
-                                order: "1",
-                                flexGrow: "0",
-                                color: "white",
-                                border: "none",
-                                cursor: "pointer",
-                            }}
-                        >
+                    <div className={styles.hz}>                       
+                        <button className={styles.continue}>
                             Продолжить
                         </button>
                     </div>

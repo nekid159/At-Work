cbb12fe74348229ef1e79690bff9bd34674dc351
Nekid159 <timur.akchl1@outlook.com>
2023-06-20 17:18:08 +0300

    finishing

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,16 +1,38 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "../assets/styles/stylesForPages/authPage/auth.module.css";
 
 const AuthPage = () => {
+    const [showPassField, setShowPassField] = useState(false);
+
+    const handleContinueClick = () => {
+        setShowPassField(true);
+    };
+
     return (
         <div className="App">
             <div className={styles.back}>
                 <div className={styles.square}>
-                    <input className={styles.email} type="text" placeholder="Введите имя пользователя"                    
-                    />
-                    <div className={styles.hz}>                       
-                        <button className={styles.continue}>
-                            Продолжить
+                    <div className={styles.switchSelect}>
+                        <div className={styles.buttonsContainer}>
+                            <button className={styles.button}>Почта</button>
+                            <button className={styles.button2}>Телефон</button>
+                        </div>
+                    </div>
+                    <input className={styles.email} type="text" placeholder="Введите имя пользователя" />
+                    {showPassField && (
+                        <>
+                            <input className={styles.email} type="text" placeholder="Введите пароль" />
+                            <div className={styles.forgotPassword}>
+                                <Link to="/forgot-password" className={styles.forgotPasswordLink}>
+                                    Забыли пароль?
+                                </Link>
+                            </div>
+                        </>
+                    )}
+                    <div className={styles.hz}>
+                        <button className={styles.continue} onClick={handleContinueClick}>
+                            Войти
                         </button>
                     </div>
                 </div>
